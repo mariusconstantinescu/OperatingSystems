e6596c2b07c134c22478ff8f93f52ac4c66daee0
Constantinescu Marius <66900748+mariusconstantinescu@users.noreply.github.com>
2021-05-20 18:05:55 +0000

    Delete Windows_freesomespace.cmd

--- Windows_freesomespace.cmd
+++
@@ -1,4 +0,0 @@
-powercfg.exe /hibernate off
-%SystemRoot%\System32\Cmd.exe /c Cleanmgr /sageset:65535 & Cleanmgr /sagerun:65535
-DISM.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
-vssadmin delete shadows /for=c: /all /quiet
